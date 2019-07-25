8f894511e7b2666e15451474f2228996061c67e4
anjana6 <anjanashakthi95@gmail.com>
2019-07-25 14:48:31 +0530

    create calculate gpa function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#subject-form');
 const subjectList = document.querySelector('.collection');
 const subjectInput = document.querySelector('#subject');
 const resultInput = document.querySelector('#result');
+const gpa = document.querySelector('#gpa');
 
 loadEventListeners();
 
@@ -13,7 +14,7 @@ function loadEventListeners(){
 
 function addDetails(e){
     if(subjectInput.value===''|| resultInput.value===''){
-        alert('File Your Result and Subject Code');
+        //alert('File Your Result and Subject Code');
     }
     const li = document.createElement('li');
     li.className = 'collection-item';
@@ -33,10 +34,78 @@ function addDetails(e){
 
     subjectList.appendChild(li);
 
-    subjectInput.value = '';
-    resultInput.value = '';
+    // subjectInput.value = '';
+    // resultInput.value = '';
 
     e.preventDefault();
+    
 
+    const totalgpa = calulateGpa();
+    // gpa.value= totalgpa;
+    // console.log(gpa.value)
+    // ;
+
+
+
+
+
+}
+
+function calulateGpa(){
+    //console.log(resultInput.value);
+    // document.getElementById("p1").value = "gpavalue";
+    //console.log(resultInput.value);
+     const gpaValue = getGpavalue(resultInput.value);
+     console.log(gpaValue);
+
+     
+    // const lastCharacterOfCode = charAt(subjectInput.length-1);
+    // const calculate = parseInt(lastCharacterOfCode)*gpaValue;
+    // return calculate;
+
+}
+
+function getGpavalue(result){
+    console.log(result);
+    switch(result){
+        case "A+":
+                gpavalue = 4.00;
+            break;
+        case "A":
+                gpavalue = 4.00;
+            break;
+        case "B+":
+                gpavalue = 4.00;
+            break;
+        case "B":
+                gpavalue = 4.00;
+            break;
+        case "B-":
+                gpavalue = 4.00;
+            break;
+        case "C+":
+                gpavalue = 4.00;
+            break;
+        case "C":
+                gpavalue = 4.00;
+            break;
+        case "C-":
+                gpavalue = 4.00;
+            break;
+        case "D+":
+                gpavalue = 4.00;
+            break;
+        case "D":
+                gpavalue = 4.00;
+            break;
+        case "E":
+                gpavalue = 4.00;
+            break;
+        default:
+                gpavalue = 0.0;
+
+    }
+    //document.getElementById("p1").value = "gpavalue";
+    return gpavalue;
 }
 
