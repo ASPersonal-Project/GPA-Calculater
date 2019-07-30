057706e3775f67edac391b6d25564fd8ee0a7b0a
anjana6 <anjanashakthi95@gmail.com>
2019-07-30 20:34:04 +0530

    update the code

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
 const form = document.querySelector('#subject-form');
-const subjectList = document.querySelector('.collection');
-const subjectInput = document.querySelector('#subject');
-const resultInput = document.querySelector('#result');
-const gpa = document.querySelector('#gpa');
-var totalgpa = 0;
-var totalcredit = 0;
+const subjectList = document.querySelector('.list-group');
+const inputSubject = document.querySelector('#inputScode');
+const inputResult = document.querySelector('#inputResult');
+const gpa = document.querySelector('#displaygpa');
+var totalGpa = 0;
+var totalCredit = 0;
 
 loadEventListeners();
 
 function loadEventListeners(){
+    
 
     form.addEventListener('submit',addDetails);
 
-    subjectList.addEventListener('click',removesubject);
+    subjectList.addEventListener('click',removeSubject);
 
 }
 
 function addDetails(e){
-    if(subjectInput.value===''|| resultInput.value===''){
-        //alert('File Your Result and Subject Code');
+    console.log("haii")
+    if(inputSubject.value===''|| inputResult.value===''){
+        alert('File Your Result and Subject Code');
     }
     const li = document.createElement('li');
-    li.className = 'collection-item';
-    const sCode = document.createElement('span');
+    li.className = 'list-group-item';
+    const Scode = document.createElement('span');
     
-    sCode.appendChild(document.createTextNode(subjectInput.value));
+    Scode.appendChild(document.createTextNode(inputSubject.value));
+    const Result = document.createElement('span');
+    Result.className = 'space';
+    Result.appendChild(document.createTextNode(inputResult.value));
+
+    
     // console.log(document.getElementsByClassName('collection-'));
     
     // sCode.appendChild(document.createTextNode(" "));
     // sCode.appendChild(document.createTextNode(resultInput.value));
-    li.appendChild(sCode);
+    li.appendChild(Scode);
+    
+    
+    li.appendChild(Result);
 
     const link = document.createElement('a');
     link.className = 'delete-item secondary-content';
@@ -41,29 +51,25 @@ function addDetails(e){
 
 
     subjectList.appendChild(li);
+    // inputSubject.value = '';
+    // inputResult.value = '';
+    
 
-    // subjectInput.value = '';
-    // resultInput.value = '';
+    
 
     e.preventDefault();
+
+    getTotalcreadit_and_valueOfLetter();
     
 
-    calulateGpa();
-    subjectInput.value = '';
-    resultInput.value = '';
+    // calulateGpa();
+    // inputSubject.value = '';
+    // inputResult.value = '';
     
-    // gpa.value= totalgpa;
-    // console.log(gpa.value)
-    // ;
-
-
-
-
-
 }
 
-function removesubject(e){
-    changeGpa();
+function removeSubject(e){
+    //changeGpa();
     
 
     if(e.target.parentElement.classList.contains('delete-item')){
@@ -78,27 +84,30 @@ function removesubject(e){
 
 }
 
-function calulateGpa(){
+function getTotalcreadit_and_valueOfLetter(){
+    //console.log(inputResult.value);
     
     
-     const gpaValue = getGpavalue(resultInput.value);
-    
+     const valueOfletter = getDigit(inputResult.value);
+     console.log(valueOfletter);
 
-    const charcter = subjectInput.value;
-    const lastCharacterOfCode = charcter.charAt(charcter.length-1);
+    const code = inputSubject.value;
+    //console.log(code);
+    const lastCharacterOfCode = code.charAt(code.length-1);
     
     
-     const calculate = parseInt(lastCharacterOfCode);
-     if(Number.isInteger(calculate)){
-         totalcredit = totalcredit+calculate;
-         //console.log(totalcredit);
-        getTotalGpa(gpaValue,totalcredit,calculate);
+     const lastNumber = parseInt(lastCharacterOfCode);
+     //console.log(lastNumber);
+     if(Number.isInteger(lastNumber)){
+         totalCredit = totalCredit+lastNumber;
+        // console.log(totalCredit);
+        getTotalGpa(valueOfletter,totalCredit,lastNumber);
          
          
      }
     //  return totalgpa;
-    //console.log(calculate);
-    // return calculate;
+    //console.log(lastNumber);
+    // return lastNumber;
 
 }
 
@@ -106,12 +115,12 @@ function changeGpa(){
     const deleteIteminput = document.querySelector('.collection-item');
     const removeCode = deleteIteminput.firstChild.innerHTML; 
     const lastCharacterOfCode = removeCode.charAt(removeCode.length-1);
-    const calculate = parseInt(lastCharacterOfCode);
-    if(Number.isInteger(calculate)){
-        totalcredit = totalcredit-calculate;
+    const lastNumber = parseInt(lastCharacterOfCode);
+    if(Number.isInteger(lastNumber)){
+        totalcredit = totalcredit-lastNumber;
         //console.log(totalcredit);
        
-        // totalgpa = totalgpa + calculate*gpaValue;
+        // totalgpa = totalgpa + lastNumber*gpaValue;
         // gpa.value= totalgpa/totalcredit;
         
     }
@@ -120,61 +129,62 @@ function changeGpa(){
         //console.log(deleteIteminput.firstChild.innerHTML);
 
 }
-function getTotalGpa(gpaValue,totalcredit,calculate){
-    console.log(gpaValue);
-    totalgpa = totalgpa + calculate*gpaValue;
-    gpa.value= totalgpa/totalcredit;
+function getTotalGpa(valueOfletter,totalCredit,lastNumber){
+    //console.log('ksnjfls');
+    //console.log(valueOfletter);
+    totalGpa = totalGpa + lastNumber*valueOfletter;
+    gpa.value= totalGpa/totalCredit;
     
 }
 
 // function totalgpa(){
-//     const totalvalue = calculateGpa();
+//     const totalvalue = lastNumberGpa();
 // }
 
-function getGpavalue(result){
+function getDigit(result){
     //console.log(result);
     switch(result){
         case "A+":
-                gpavalue = 4.00;
+                digit = 4.00;
             break;
         case "A":
-                gpavalue = 4.00;
+                digit = 4.00;
             break;
         case "A-":
-                gpavalue = 3.70;
+                digit = 3.70;
             break;
         case "B+":
-                gpavalue = 3.30;
+                digit = 3.30;
             break;
         case "B":
-                gpavalue = 3.00;
+                digit = 3.00;
             break;
         case "B-":
-                gpavalue = 2.70;
+                digit = 2.70;
             break;
         case "C+":
-                gpavalue = 2.30;
+                digit = 2.30;
             break;
         case "C":
-                gpavalue = 2.00;
+                digit = 2.00;
             break;
         case "C-":
-                gpavalue = 1.70;
+                digit = 1.70;
             break;
         case "D+":
-                gpavalue = 1.30;
+                digit = 1.30;
             break;
         case "D":
-                gpavalue = 1.00;
+                digit = 1.00;
             break;
         case "E":
-                gpavalue = 0.00;
+                digit = 0.00;
             break;
         default:
-                gpavalue = 0.0;
+                digit = 0.0;
 
     }
     //document.getElementById("p1").value = "gpavalue";
-    return gpavalue;
+    return  digit;
 }
 
